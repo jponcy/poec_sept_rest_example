f1b36df9d94137c27bbe12cd5dd0e23ab13d35da
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-11-23 15:27:03 +0100

    Simplify success code

--- src/main/java/com/tact/poec/todo/FakeAndBadTodoItemController.java
+++ src/main/java/com/tact/poec/todo/FakeAndBadTodoItemController.java
@@ -1,23 +1,20 @@
 
 package com.tact.poec.todo;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -52,32 +49,13 @@ public class FakeAndBadTodoItemController {
     }
 
     @PostMapping("todo")
-    public Object create(@Valid @RequestBody final TodoItemUpdateDTO dto, final HttpServletResponse response) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public TodoItem create(@Valid @RequestBody final TodoItemUpdateDTO dto) {
         final TodoItem item = new TodoItem(dto.getLabel());
 
         this.repository.save(item);
 
-        if (true == this.hack()) {
-            throw new MyBadRequest("Label manquant");
-        }
-        response.setStatus(HttpStatus.CREATED.value());
-
         return item;
-    }
-
-    private boolean hack() {
-        return true;
-    }
-
-    public class MyBadRequest extends RuntimeException {
-        public MyBadRequest(final String msg) {
-            super(msg);
-        }
-    }
-
-    @ExceptionHandler(MyBadRequest.class)
-    public ResponseEntity<Map<String, Object>> toto(final MyBadRequest m, final HttpServletResponse response) {
-        return ResponseEntity.badRequest().body(Collections.singletonMap("error", m.getMessage()));
     }
 
     // @DeleteMapping // No, due to not real deletion, except if we want declare is delete even if it is not.
